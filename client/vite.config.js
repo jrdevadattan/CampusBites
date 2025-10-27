219acbf38c2c1946be63acb5c1340b13c7807b7a
J R Deva Dattan <jrdevadattan2006@gmail.com>
2025-10-27 17:00:12 +0530

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Add these headers so popups (like Google login) can send messages back
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
+    },
+  },
+
+  // (optional) If you sometimes run your backend locally
+  // and want frontend requests to automatically proxy
+  // /api calls to backend
+  // proxy: {
+  //   '/api': 'http://localhost:5000',
+  // },
 })
